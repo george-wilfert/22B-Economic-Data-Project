0d4bbbbb0f70d394dc1aef95e76ac933de9fe1f8
George Wilfert <151199530+george-wilfert@users.noreply.github.com>
2024-04-29 15:37:46 -0700

    Create projectfile.py

---
+++ projectfile.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets
+from sklearn import linear_model
+import mpl_toolkits.mplot3d
+from datetime import *
+from mpl_toolkits.mplot3d import Axes3D
+
+#3d Model
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import mpl_toolkits.mplot3d
