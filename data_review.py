1e96a08b41c861c21073e3ccada274a95896a569
George Wilfert <151199530+george-wilfert@users.noreply.github.com>
2024-04-30 14:24:16 -0700

    Update data_review.py

--- data_review.py
+++ data_review.py
@@ -2,5 +2,10 @@
 data = pd.read_csv("data.csv")
 meta_data = pd.read_csv("metadata.csv")
 
-#printing information about data and meta data
+#data quick stats
 print(data.time_series_code.value_counts())
+print(data.info())
+print(data.describe())
+print(data.shape)
+print(data.size)
+print(data.memory_usage)
