f28be115888161376e0961329b0eb7265884fa69
George Wilfert <151199530+george-wilfert@users.noreply.github.com>
2024-04-30 14:13:08 -0700

    Update data_review.py

--- data_review.py
+++ data_review.py
@@ -1,3 +1,6 @@
-import pandas as pd
-import seaborn as sns
+#reading in data and metadata
+data = pd.read_csv("data.csv")
+meta_data = pd.read_csv("metadata.csv")
 
+#printing information about data and meta data
+print(data.time_series_code.value_counts())
