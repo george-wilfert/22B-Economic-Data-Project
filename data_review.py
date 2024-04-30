06914f0284011df74dd3954614e86c78e230eab9
George Wilfert <151199530+george-wilfert@users.noreply.github.com>
2024-04-30 14:24:47 -0700

    Update data_review.py

--- data_review.py
+++ data_review.py
@@ -9,3 +9,11 @@ print(data.describe())
 print(data.shape)
 print(data.size)
 print(data.memory_usage)
+
+#meta_data quick stats
+print(meta_data.value_counts())
+print(meta_data.info())
+print(meta_data.describe())
+print(meta_data.shape)
+print(meta_data.size)
+print(meta_data.memory_usage)
